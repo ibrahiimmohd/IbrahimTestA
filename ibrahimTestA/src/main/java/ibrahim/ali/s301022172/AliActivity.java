c089995e7b6faf1d00505031898a82a47176689d
Ibrahim Ali <47118191+ibrahiimmohd@users.noreply.github.com>
2020-10-31 08:49:44 -0400

    Initial commit

---
+++ ibrahimTestA/src/main/java/ibrahim/ali/s301022172/AliActivity.java
@@ -0,0 +1,20 @@
+/**
+ * Full Name: Ibrahim Ali
+ * Student ID: 301022172
+ * Section: COMP 304 - 002
+ * */
+
+package ibrahim.ali.s301022172;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AliActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ali);
+    }
+}
